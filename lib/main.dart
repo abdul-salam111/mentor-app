2f769da1b6d67aff867c7b0244a0d85215e67e86
Abdul Salam <151502342+SaeedanTechnologies@users.noreply.github.com>
2024-03-01 12:00:18 +0500

    gekki

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Mentor App",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+    ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Mentor App",
+          initialRoute: Routes.HOME,
+          getPages: AppPages.routes,
+        );
+      },
     ),
   );
 }
